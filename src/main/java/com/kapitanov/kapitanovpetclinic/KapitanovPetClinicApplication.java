af4e9769f59c2df2a4e410a9dfa3ed63f6956c47
Yani Kapitanov <yani.kapitanov@amadeus.com>
2019-12-17 09:54:10 +0100

    -asd

---
+++ src/main/java/com/kapitanov/kapitanovpetclinic/KapitanovPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.kapitanov.kapitanovpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KapitanovPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KapitanovPetClinicApplication.class, args);
+	}
+
+}

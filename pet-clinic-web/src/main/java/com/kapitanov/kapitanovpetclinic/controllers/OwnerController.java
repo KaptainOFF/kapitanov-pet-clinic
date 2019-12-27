27f7e9c6a8d1c029a0a0c4ab15fc57303a44d9b5
yk <yani.kapitanov@gmail.com>
2019-12-27 19:45:53 +0100

    Added controller and index.html. Closes #8

---
+++ pet-clinic-web/src/main/java/com/kapitanov/kapitanovpetclinic/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package com.kapitanov.kapitanovpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+
+    @RequestMapping({ "", "/", "/index", "/index.html"})
+    public String owners() {
+        return "owners/index";
+    }
+}

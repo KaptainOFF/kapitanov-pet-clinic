daf3c5836f6c10bdb63b4e633a82d49e15ae68d3
yk <yani.kapitanov@gmail.com>
2019-12-30 11:20:52 +0100

    Display all vets on vets index page and owners on owners index page

--- pet-clinic-web/src/main/java/com/kapitanov/kapitanovpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/kapitanov/kapitanovpetclinic/controllers/OwnerController.java
@@ -1,14 +1,27 @@
 package com.kapitanov.kapitanovpetclinic.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kapitanov.kapitanovpetclinic.service.OwnerService;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
-    @RequestMapping({ "", "/", "/index", "/index.html"})
-    public String owners() {
-        return "owners/index";
-    }
+	private final OwnerService ownerService;
+	
+	@Autowired
+	public OwnerController(OwnerService ownerService) {
+		super();
+		this.ownerService = ownerService;
+	}
+
+	@RequestMapping({ "", "/", "/index", "/index.html" })
+	public String owners(Model model) {
+		model.addAttribute("owners", ownerService.findAll());
+		return "owners/index";
+	}
 }
--- pet-clinic-web/src/main/java/com/kapitanov/kapitanovpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/kapitanov/kapitanovpetclinic/controllers/VetController.java
@@ -1,13 +1,25 @@
 package com.kapitanov.kapitanovpetclinic.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-public class VetController {
+import com.kapitanov.kapitanovpetclinic.service.VetService;
 
-    @RequestMapping({"/vets","/vets/index", "/vets/index.html"})
-    public String listVets() {
+@Controller
+@RequestMapping("/vets")
+public class VetController {
+	 private final VetService vetService;
+	 
+	 @Autowired
+    public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
+
+	@RequestMapping({"","/","/index", "/index.html"})
+    public String listVets(Model model) {
+		model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
